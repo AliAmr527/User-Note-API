1db51746431f793aa4c3090ac4eb88613791ccc1
Ali El-Mallah <131396543+AliAmr527@users.noreply.github.com>
2023-07-12 17:17:05 +0300

    Delete User Note Apis directory

--- User Note Apis/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import bootstrap from "./src/index.router.js"
-const app = express()
-const port = 5000
-
-bootstrap(app, express)
-app.listen(port, () => {
-    console.log(`server listening on port...........${port}`)
-})
